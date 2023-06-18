7d0264040addd23a5897328ef3cf61d947538e02
Rafael Araujo <rafa123839@gmail.com>
2023-06-18 16:30:47 -0300

    finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import styles from './header.module.scss'
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.contentContainer}>
-        <img src="/images/logo.svg" alt="spacetraveling" />
+        <Link href="/">
+        <img src="/images/logo.svg" alt="logo" />
+        </Link>
       </div>
     </header>
   )
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,13 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
+import * as prismicHelpers from '@prismicio/helpers';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 import Header from '../../components/Header';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -28,7 +30,12 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post({ post }: PostProps) {
+export default function Post({ post }: PostProps) : JSX.Element{
+
+  post.data.content.map(content => {
+    console.log(content.body[0].text);
+  })
+
   return (
     <>
       <Header />
@@ -49,11 +56,18 @@ export default function Post({ post }: PostProps) {
             </div>
           </div>
         </div>
+
         <div className={styles.content}>
           <div className={styles.heading}>
-
+            {post.data.content.map(content => (
+              <div className={styles.postContent} key={content.heading}>
+                <h1>{content.heading}</h1>
+                <p>{content.body[0].text}</p>
+              </div>
+            ))}
           </div>
         </div>
+        
       </main>
     </>
   )
@@ -74,6 +88,10 @@ export const getStaticProps = async ({ params }) => {
   const prismic = getPrismicClient({});
   const response = await prismic.getByUID('posts', slug);
   console.log(JSON.stringify(response, undefined, 2))
+  console.log('--------------------------------------------')
+
+  // console.log(prismicHelpers.asHTML(response.data.content))
+
 
   const post = {
     first_publication_date: new Date(response.first_publication_date).toLocaleDateString('pt-BR', {
@@ -87,23 +105,27 @@ export const getStaticProps = async ({ params }) => {
         url: response.data.banner.url,
       },
       author: response.data.author.find((author: any) => author.type === 'paragraph')?.text ?? '',
-
-      content: {
-        heading: response.data.content.map(post => {
-          return post.heading.find((heading: any) => heading.type === 'paragraph')?.text ?? ''
-        })[0],
-        body: {
-          text: response.data.content.map(post => {
-            return post.body.find((body: any) => body.type === 'paragraph')?.text ?? ''
+      content: [
+        {
+          heading: response.data.content.map(post => {
+            return post.heading.find((heading: any) => heading.type === 'paragraph')?.text ?? ''
           })[0],
+          body: [
+            {
+              text: response.data.content.map(post => {
+                return post.body.find((body: any) => body.type === 'paragraph')?.text ?? ''
+              })[0],
+            }
+          ]
         }
-      }
+      ]
+      // content: prismicHelpers.asHTML(response.data.content)
     },
   }
 
-  console.log("******************************")
+  // console.log("******************************")
 
-  console.log(JSON.stringify(post, undefined, 2))
+  // console.log(JSON.stringify(post, undefined, 2))
   return {
     props: { post }
   }
